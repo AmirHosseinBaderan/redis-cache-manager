0f8193ab596f66436319900c9dcaa3629e76f712
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-06-29 10:30:54 +0330

    create : cache duration model

---
+++ RedisCacheManager/Src/RedisCacheManager/Configuration/CacheDuration.cs
@@ -0,0 +1,11 @@
+﻿namespace RedisCacheManager.Configuration;
+
+public record CacheDuration(int Milliseconds = 0, int Second = 0, int Minute = 0, int Hour = 0, int Day = 0)
+{
+    public TimeSpan ToTimeSpan()
+        => new(days: Day,
+            hours: Hour,
+            minutes: Minute,
+            seconds: Second,
+            milliseconds: Milliseconds);
+}
