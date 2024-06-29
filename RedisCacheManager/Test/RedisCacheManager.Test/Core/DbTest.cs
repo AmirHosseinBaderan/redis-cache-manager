10e0824d15c95a95ded68f7df2ce40dcb8d20e7a
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-06-29 18:17:10 +0330

    create : cache db test

---
+++ RedisCacheManager/Test/RedisCacheManager.Test/Core/DbTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RedisCacheManager.Configuration;
+using RedisCacheManager.Core;
+
+namespace RedisCacheManager.Test.Core;
+
+public class DbTest
+{
+    private IServiceCollection _services;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _services = new ServiceCollection();
+        _services.AddRedisCacheManager(() => new("127.0.0.1:6379", 1));
+    }
+
+    public async Task<ICacheDb?> GetDbService()
+    {
+        IServiceProvider provider = _services.BuildServiceProvider();
+        return provider.GetService<ICacheDb?>();
+    }
+
+    [Test]
+    public async Task GetDataBaseInstanceWithDefaultConfig()
+    {
+        var service = await GetDbService();
+        if (service is null)
+        {
+            Assert.Fail("Cant inject services");
+            return;
+        }
+
+        var db = await service.GetDataBaseAsync();
+        if (db is null)
+        {
+            Assert.Fail("Cant open data base with default configs");
+            return;
+        }
+
+        Assert.Pass("Data base opend successfuly with default configs");
+    }
+
+    [Test]
+    public async Task GetDataBaseInstanceWithCustomeConfig()
+    {
+        var service = await GetDbService();
+        if (service is null)
+        {
+            Assert.Fail("Cant inject services");
+            return;
+        }
+
+        var db = await service.GetDataBaseAsync("127.0.0.1:6379", 1);
+        if (db is null)
+        {
+            Assert.Fail("Cant open data base with custome configs");
+            return;
+        }
+
+        Assert.Pass("Data base opend successfuly with custome configs");
+    }
+}
