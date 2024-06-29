25f54460b9f931414718e83c07fd49cdfa4fbdf0
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-06-29 18:36:05 +0330

    create : cache test

---
+++ RedisCacheManager/Test/RedisCacheManager.Test/Cache/CacheTest.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RedisCacheManager.Abstraction;
+using RedisCacheManager.Configuration;
+
+namespace RedisCacheManager.Test.Cache;
+
+public class CacheTest
+{
+    private IServiceCollection _services;
+
+    private CacheModel _model;
+
+    private readonly string _key = "Cached-Item";
+
+    [SetUp]
+    public void SetUp()
+    {
+        _model = new("1", "Amir", "Baderan");
+
+        _services = new ServiceCollection();
+        _services.AddRedisCacheManager(() => new("127.0.0.1:6379", 1));
+    }
+
+    public async Task<ICache?> GetService()
+    {
+        IServiceProvider provider = _services.BuildServiceProvider();
+        return provider.GetService<ICache?>();
+    }
+
+    [Test]
+    public async Task SetCache()
+    {
+        var service = await GetService();
+        if (service is null)
+        {
+            Assert.Fail("Cant inject services");
+            return;
+        }
+
+        var result = await service.SetItemAsync(_key, _model);
+        if (result != _model)
+        {
+            Assert.Fail("Set item fail");
+            return;
+        }
+
+        Assert.Pass("Item set successfuly");
+        return;
+    }
+
+    [Test]
+    public async Task GetCache()
+    {
+        var service = await GetService();
+        if (service is null)
+        {
+            Assert.Fail("Cant inject services");
+            return;
+        }
+
+        var result = await service.GetItemAsync<CacheModel>(_key);
+        if (result != _model)
+        {
+            Assert.Fail("Get item fail");
+            return;
+        }
+
+        Assert.Pass("Item Get successfuly");
+        return;
+    }
+
+    [Test]
+    public async Task GetOrSetCache()
+    {
+        var service = await GetService();
+        if (service is null)
+        {
+            Assert.Fail("Cant inject services");
+            return;
+        }
+
+        var result = await service.GetOrderSetItemAsync(_key + "GetOrSet", () =>
+        {
+            CacheModel model = new("2", "Amir2", "Baderan2");
+            return model;
+        });
+        if (result is not CacheModel { Id: "2", Name: "Amir2", LastName: "Baderan2" })
+        {
+            Assert.Fail("Get or set item fail");
+            return;
+        }
+
+        Assert.Pass("Item Get or set successfuly");
+        return;
+    }
+}
---
+++ RedisCacheManager/Test/RedisCacheManager.Test/Cache/Model.cs
@@ -0,0 +1,3 @@
+﻿namespace RedisCacheManager.Test.Cache;
+
+public record CacheModel(string Id, string Name, string LastName);
