322961d9522076d11b21190b463af19f0120c112
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-06-29 09:58:07 +0330

    create : cache core

---
+++ RedisCacheManager/Src/RedisCacheManager/Core/CacheCore.cs
@@ -0,0 +1,35 @@
+﻿using RedisCacheManager.Configuration;
+using StackExchange.Redis;
+
+namespace RedisCacheManager.Core;
+
+internal class CacheCore : ICacheCore
+{
+    private ConnectionMultiplexer? _connection;
+
+    public async Task<ConnectionMultiplexer?> ConnectAsync()
+        => await ConnectAsync(Configs.ConnectionString);
+
+    public async Task<ConnectionMultiplexer?> ConnectAsync(string connectionString)
+    {
+        try
+        {
+            if (_connection is not null and { IsConnected: true })
+                return _connection;
+
+            _connection = await ConnectionMultiplexer.ConnectAsync(connectionString);
+            return _connection;
+        }
+        catch
+        {
+            throw;
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        if (_connection is not null)
+            await _connection.DisposeAsync();
+    }
+}
---
+++ RedisCacheManager/Src/RedisCacheManager/Core/ICacheCore.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+
+namespace RedisCacheManager.Core;
+
+internal interface ICacheCore : IAsyncDisposable
+{
+    /// <summary>
+    /// Create connection with redis server use default configuration
+    /// </summary>
+    /// <returns><see cref="ConnectionMultiplexer"/> Instance </returns>
+    Task<ConnectionMultiplexer?> ConnectAsync();
+
+    /// <summary>
+    /// Create connection with redis server use connection string
+    /// </summary>
+    /// <param name="connectionString">Connection string for connect to redis</param>
+    /// <returns><see cref="ConnectionMultiplexer"/> Instance </returns>
+    Task<ConnectionMultiplexer?> ConnectAsync(string connectionString);
+}
