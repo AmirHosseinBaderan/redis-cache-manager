a57667ff0f3ceef7b6d1750981b916e66edb7569
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-06-29 10:48:48 +0330

    create : cache db manager

---
+++ RedisCacheManager/Src/RedisCacheManager/Core/DataBase/CacheDb.cs
@@ -0,0 +1,26 @@
+﻿using RedisCacheManager.Configuration;
+using StackExchange.Redis;
+
+namespace RedisCacheManager.Core;
+
+internal class CacheDb(ICacheCore core) : ICacheDb
+{
+    public async Task<IDatabase?> GetDataBaseAsync()
+        => await GetDataBaseAsync(Configs.CacheConfigs.ConnectionString);
+
+    public async Task<IDatabase?> GetDataBaseAsync(string connectionString)
+    {
+        try
+        {
+            var connection = await core.ConnectAsync(connectionString);
+            if (connection is null)
+                return null;
+
+            return connection.GetDatabase(Configs.CacheConfigs.Instance);
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
---
+++ RedisCacheManager/Src/RedisCacheManager/Core/DataBase/ICacheDb.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+
+namespace RedisCacheManager.Core;
+
+internal interface ICacheDb
+{
+    /// <summary>
+    /// Create connection and open data base using default configuration
+    /// </summary>
+    /// <returns> Instance of <see cref="IDatabase"/></returns>
+    Task<IDatabase?> GetDataBaseAsync();
+
+    /// <summary>
+    /// Create connection and open data base using connection string 
+    /// </summary>
+    /// <param name="connectionString">Connection string for connect to redis</param>
+    /// <returns>Instance of <see cref="IDatabaseAsync"/></returns>
+    Task<IDatabase?> GetDataBaseAsync(string connectionString);
+}
