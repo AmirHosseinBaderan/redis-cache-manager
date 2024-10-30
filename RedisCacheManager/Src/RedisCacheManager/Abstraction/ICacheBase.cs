d19bd25e4be85d5e420c782047fa8fc5d4431c41
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-10-30 17:52:46 +0330

    create : cache base

---
+++ RedisCacheManager/Src/RedisCacheManager/Abstraction/ICacheBase.cs
@@ -0,0 +1,22 @@
+﻿namespace RedisCacheManager.Abstraction;
+
+internal interface ICacheBase : IAsyncDisposable
+{
+    Task<RedisValue?> GetItemAsync(string key);
+
+    Task<RedisValue?> SetItemAsync(string key, RedisValue? obj);
+
+    Task<RedisValue?> SetItemAsync(string key, RedisValue? obj, CacheDuration duration);
+
+    Task<RedisValue?> SetItemIfAsync(bool condition, string key, RedisValue? obj);
+
+    Task<RedisValue?> SetItemIfAsync(bool condition, string key, RedisValue? obj, CacheDuration duration);
+
+    Task RemoveItemAsync(string key);
+
+    Task<RedisValue?> GetOrderSetItemAsync(string key, Func<Task<RedisValue>> action);
+
+    Task<RedisValue?> GetOrderSetItemAsync(string key, CacheDuration cacheDuration, Func<Task<RedisValue>> action);
+
+    Task<RedisValue?> SetItemAsync(string key, RedisValue obj, TimeSpan? cacheTime);
+}
---
+++ RedisCacheManager/Src/RedisCacheManager/Implementation/CacheBase.cs
@@ -0,0 +1,57 @@
+﻿
+
+namespace RedisCacheManager.Implementation;
+
+internal class CacheBase(ICacheDb cacheDb) : ICacheBase
+{
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        await cacheDb.DisposeAsync();
+    }
+
+    public Task<RedisValue?> GetItemAsync(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RedisValue?> GetOrderSetItemAsync(string key, Func<Task<RedisValue>> action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RedisValue?> GetOrderSetItemAsync(string key, CacheDuration cacheDuration, Func<Task<RedisValue>> action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveItemAsync(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RedisValue?> SetItemAsync(string key, RedisValue? obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RedisValue?> SetItemAsync(string key, RedisValue? obj, CacheDuration duration)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RedisValue?> SetItemAsync(string key, RedisValue obj, TimeSpan? cacheTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RedisValue?> SetItemIfAsync(bool condition, string key, RedisValue? obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RedisValue?> SetItemIfAsync(bool condition, string key, RedisValue? obj, CacheDuration duration)
+    {
+        throw new NotImplementedException();
+    }
+}
