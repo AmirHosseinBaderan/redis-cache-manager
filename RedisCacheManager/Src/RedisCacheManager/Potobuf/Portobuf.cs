3973f2cc8fdb1cbe54617557dfd10728b81fd69e
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-10-30 14:58:53 +0330

    create : portobuf tools

---
+++ RedisCacheManager/Src/RedisCacheManager/Potobuf/Portobuf.cs
@@ -0,0 +1,19 @@
+﻿using Google.Protobuf;
+
+namespace RedisCacheManager;
+
+public static class RedisPortobuf
+{
+    public static byte[] Serialize<T>(this T obj) where T : IMessage<T>
+    {
+        using MemoryStream ms = new();
+        obj.WriteTo(ms);
+        return ms.ToArray();
+    }
+
+    public static T Deserialize<T>(this byte[] data) where T : IMessage<T>, new()
+    {
+        T model = new();
+        return (T)model.Descriptor.Parser.ParseFrom(data);
+    }
+}
