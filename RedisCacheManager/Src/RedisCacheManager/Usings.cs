d06c08857f63f8ac861efd4d29f77bbb2aa24d71
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-10-30 15:26:42 +0330

    Create Usings.cs

---
+++ RedisCacheManager/Src/RedisCacheManager/Usings.cs
@@ -0,0 +1,6 @@
+﻿global using Google.Protobuf;
+global using Newtonsoft.Json;
+global using RedisCacheManager.Abstraction;
+global using RedisCacheManager.Configuration;
+global using RedisCacheManager.Core;
+global using StackExchange.Redis;
