4eb53732a434aae1d76f446ea7810848a409d8df
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-10-31 20:11:58 +0330

    update : add config

--- RedisCacheManager/Src/RedisCacheManager/Configuration/Configs.cs
+++ RedisCacheManager/Src/RedisCacheManager/Configuration/Configs.cs
@@ -1,8 +1,14 @@
 ﻿namespace RedisCacheManager.Configuration;
 
-public record CacheConfigs(string ConnectionString, int Instance);
+public record CacheConfigs(string ConnectionString, int Instance, CacheType Type = CacheType.Json);
 
 public class Configs
 {
     public static CacheConfigs CacheConfigs { get; set; } = null!;
+}
+
+public enum CacheType
+{
+    Json,
+    Proto
 }
