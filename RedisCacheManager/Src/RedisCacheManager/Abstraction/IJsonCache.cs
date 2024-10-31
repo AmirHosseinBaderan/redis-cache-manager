536ea6cf5d2a0dd2372febba838c6092d3036085
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-10-31 19:28:27 +0330

    create : json cache

---
+++ RedisCacheManager/Src/RedisCacheManager/Abstraction/IJsonCache.cs
@@ -0,0 +1,26 @@
+﻿namespace RedisCacheManager.Abstraction;
+
+public interface IJsonCache
+{
+    Task<TModel?> GetItemAsync<TModel>(string key);
+
+    Task<TModel?> SetItemAsync<TModel>(string key, TModel? obj);
+
+    Task<TModel?> SetItemAsync<TModel>(string key, TModel? obj, CacheDuration duration);
+
+    Task<TModel?> SetItemIfAsync<TModel>(bool condition, string key, TModel? obj);
+
+    Task<TModel?> SetItemIfAsync<TModel>(bool condition, string key, TModel? obj, CacheDuration duration);
+
+    Task RemoveItemAsync(string key);
+
+    Task<TModel?> GetOrderSetItemAsync<TModel>(string key, Func<Task<TModel>> action);
+
+    Task<TModel?> GetOrderSetItemAsync<TModel>(string key, Func<TModel> action);
+
+    Task<TModel?> GetOrderSetItemAsync<TModel>(string key, CacheDuration cacheDuration, Func<Task<TModel>> action);
+
+    Task<TModel?> GetOrderSetItemAsync<TModel>(string key, CacheDuration cacheDuration, Func<TModel> action);
+
+    Task<TModel?> SetItemAsync<TModel>(string key, TModel obj, TimeSpan? cacheTime);
+}
---
+++ RedisCacheManager/Src/RedisCacheManager/Implementation/JsonCache.cs
@@ -0,0 +1,140 @@
+﻿namespace RedisCacheManager.Implementation;
+
+internal class JsonCache(ICacheBase cacheBase) : IJsonCache
+{
+    public async Task<TModel?> GetItemAsync<TModel>(string key)
+    {
+        try
+        {
+            RedisValue value = await cacheBase.GetItemAsync(key);
+            if (value.IsNullOrEmpty)
+                return default;
+            return JsonConvert.DeserializeObject<TModel>(value.ToString());
+        }
+        catch
+        {
+            return default;
+        }
+    }
+
+    public async Task<TModel?> GetOrderSetItemAsync<TModel>(string key, Func<Task<TModel>> func)
+    {
+        try
+        {
+            RedisValue value = await cacheBase.GetOrderSetItemAsync(key, async () =>
+            {
+                TModel? res = await func();
+                if (res is null)
+                    return RedisValue.Null;
+                string json = JsonConvert.SerializeObject(res);
+                return new(json);
+            });
+            return value.IsNullOrEmpty
+                ? await func()
+                : JsonConvert.DeserializeObject<TModel>(value.ToString());
+        }
+        catch
+        {
+            return await func();
+        }
+    }
+
+    public async Task<TModel?> GetOrderSetItemAsync<TModel>(string key, CacheDuration cacheDuration, Func<Task<TModel>> func)
+    {
+        try
+        {
+            RedisValue value = await cacheBase.GetOrderSetItemAsync(key, cacheDuration, async () =>
+            {
+                TModel? res = await func();
+                if (res is null)
+                    return RedisValue.Null;
+                string json = JsonConvert.SerializeObject(res);
+                return new(json);
+            });
+            return value.IsNullOrEmpty
+                ? await func()
+                : JsonConvert.DeserializeObject<TModel>(value.ToString());
+        }
+        catch
+        {
+            return await func();
+        }
+    }
+
+    public async Task<TModel?> GetOrderSetItemAsync<TModel>(string key, Func<TModel> action)
+    {
+        try
+        {
+            RedisValue value = await cacheBase.GetOrderSetItemAsync(key, () =>
+            {
+                TModel? res = action();
+                if (res is null)
+                    return RedisValue.Null;
+                string json = JsonConvert.SerializeObject(res);
+                return new(json);
+            });
+            return value.IsNullOrEmpty
+                ? action()
+                : JsonConvert.DeserializeObject<TModel>(value.ToString());
+        }
+        catch
+        {
+            return action();
+        }
+    }
+
+    public async Task<TModel?> GetOrderSetItemAsync<TModel>(string key, CacheDuration cacheDuration, Func<TModel> action)
+    {
+        try
+        {
+            RedisValue value = await cacheBase.GetOrderSetItemAsync(key, cacheDuration, () =>
+            {
+                TModel? res = action();
+                if (res is null)
+                    return RedisValue.Null;
+                string json = JsonConvert.SerializeObject(res);
+                return new(json);
+            });
+            return value.IsNullOrEmpty
+                ? action()
+                : JsonConvert.DeserializeObject<TModel>(value.ToString());
+        }
+        catch
+        {
+            return action();
+        }
+    }
+
+    public async Task RemoveItemAsync(string key)
+        => await cacheBase.RemoveItemAsync(key);
+
+    public async Task<TModel?> SetItemAsync<TModel>(string key, TModel? obj)
+        => await SetItemAsync(key, obj, cacheTime: null);
+
+    public async Task<TModel?> SetItemAsync<TModel>(string key, TModel? obj, CacheDuration duration)
+        => await SetItemAsync(key, obj, duration.ToTimeSpan());
+
+    public async Task<TModel?> SetItemAsync<TModel>(string key, TModel obj, TimeSpan? cacheTime)
+    {
+        try
+        {
+            string json = JsonConvert.SerializeObject(obj);
+            await cacheBase.SetItemAsync(key, new(json), cacheTime);
+            return obj;
+        }
+        catch
+        {
+            return obj;
+        }
+    }
+
+    public async Task<TModel?> SetItemIfAsync<TModel>(bool condition, string key, TModel? obj)
+        => condition ?
+          await SetItemAsync(key, obj, cacheTime: null)
+        : obj;
+
+    public async Task<TModel?> SetItemIfAsync<TModel>(bool condition, string key, TModel? obj, CacheDuration duration)
+        => condition ?
+          await SetItemAsync(key, obj, cacheTime: duration.ToTimeSpan())
+        : obj;
+}
