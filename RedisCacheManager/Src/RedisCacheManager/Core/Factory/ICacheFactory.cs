c750ce6d5127848d1545d37cd2979f3c5a1034f4
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-11-01 01:05:29 +0330

    delete : useless factory

--- RedisCacheManager/Src/RedisCacheManager/Core/Factory/ICacheFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RedisCacheManager.Core.Factory;
-
-public interface ICacheFactory
-{
-    ICache<TModel> CreateJsonCache<TModel>();
-
-    ICache<TModel> CreateProtoCache<TModel>() where TModel : IMessage<TModel>, new();
-}
