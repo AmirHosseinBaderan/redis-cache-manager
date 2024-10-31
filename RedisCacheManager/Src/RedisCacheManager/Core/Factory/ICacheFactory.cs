870d56ab31e53369a16665f509d4744f3144958f
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-11-01 00:33:06 +0330

    create : cache factory

---
+++ RedisCacheManager/Src/RedisCacheManager/Core/Factory/ICacheFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace RedisCacheManager.Core.Factory;
+
+public interface ICacheFactory
+{
+    ICache<TModel> CreateJsonCache<TModel>();
+
+    ICache<TModel> CreateProtoCache<TModel>() where TModel : IMessage<TModel>, new();
+}
