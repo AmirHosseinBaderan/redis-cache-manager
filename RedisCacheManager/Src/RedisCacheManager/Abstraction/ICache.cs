4846b212895215f9c641c42592b5174157589c23
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-10-31 20:11:31 +0330

    update : use global generic

--- RedisCacheManager/Src/RedisCacheManager/Abstraction/ICache.cs
+++ RedisCacheManager/Src/RedisCacheManager/Abstraction/ICache.cs
@@ -1,26 +1,26 @@
 ﻿namespace RedisCacheManager.Abstraction;
 
-public interface ICache
+public interface ICache<TModel>
 {
-    Task<TModel?> GetItemAsync<TModel>(string key);
+    Task<TModel?> GetItemAsync(string key);
 
-    Task<TModel?> SetItemAsync<TModel>(string key, TModel? obj);
+    Task<TModel?> SetItemAsync(string key, TModel? obj);
 
-    Task<TModel?> SetItemAsync<TModel>(string key, TModel? obj, CacheDuration duration);
+    Task<TModel?> SetItemAsync(string key, TModel? obj, CacheDuration duration);
 
-    Task<TModel?> SetItemIfAsync<TModel>(bool condition, string key, TModel? obj);
+    Task<TModel?> SetItemIfAsync(bool condition, string key, TModel? obj);
 
-    Task<TModel?> SetItemIfAsync<TModel>(bool condition, string key, TModel? obj, CacheDuration duration);
+    Task<TModel?> SetItemIfAsync(bool condition, string key, TModel? obj, CacheDuration duration);
 
     Task RemoveItemAsync(string key);
 
-    Task<TModel?> GetOrderSetItemAsync<TModel>(string key, Func<Task<TModel>> action);
+    Task<TModel?> GetOrderSetItemAsync(string key, Func<Task<TModel>> action);
 
-    Task<TModel?> GetOrderSetItemAsync<TModel>(string key, Func<TModel> action);
+    Task<TModel?> GetOrderSetItemAsync(string key, Func<TModel> action);
 
-    Task<TModel?> GetOrderSetItemAsync<TModel>(string key, CacheDuration cacheDuration, Func<Task<TModel>> action);
+    Task<TModel?> GetOrderSetItemAsync(string key, CacheDuration cacheDuration, Func<Task<TModel>> action);
 
-    Task<TModel?> GetOrderSetItemAsync<TModel>(string key, CacheDuration cacheDuration, Func<TModel> action);
+    Task<TModel?> GetOrderSetItemAsync(string key, CacheDuration cacheDuration, Func<TModel> action);
 
-    Task<TModel?> SetItemAsync<TModel>(string key, TModel obj, TimeSpan? cacheTime);
+    Task<TModel?> SetItemAsync(string key, TModel? obj, TimeSpan? cacheTime);
 }
